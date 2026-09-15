52c86d303c71c85ac60ad981669a4a7fba57f11c
chaminade <thibault.chaminade@aplim.fr>
2026-09-15 15:23:13 +0200

    employee mapping

---
+++ Code/Cantine/CantineServiceFromBDD/Mappings/EmployeMapping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using CantineServiceFromBDD.Models;
+using HRDAL.DAO;
+
+namespace CantineServiceFromBDD.Mappings
+{
+    public static class EmployeMapping
+    {
+        public static void AddEmployeMapping(this IMapperConfigurationExpression config) {
+
+            // Lecture : la BDD vers le métier
+            config.CreateMap<EmployeDAO, Employe>()
+                         .ForMember(c => c.Matricule, o => o.MapFrom(c => c.PublicId))
+                          .ForMember(c => c.DateNaissance, o => o.MapFrom(c => c.BirthDate))
+                           .ForMember(c => c.Nom, o => o.MapFrom(c => c.Name))
+                            .ForMember(c => c.Prenom, o => o.MapFrom(c => c.Surname));
+        }
+    }
+}
