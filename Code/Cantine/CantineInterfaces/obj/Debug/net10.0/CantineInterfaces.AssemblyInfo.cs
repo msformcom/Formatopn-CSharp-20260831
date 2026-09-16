05b3ac748f1f04fc87397b2e7831349ecb926d16
chaminade <thibault.chaminade@aplim.fr>
2026-09-16 13:28:56 +0200

    config

--- Code/Cantine/CantineInterfaces/obj/Debug/net10.0/CantineInterfaces.AssemblyInfo.cs
+++ Code/Cantine/CantineInterfaces/obj/Debug/net10.0/CantineInterfaces.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantineInterfaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579d4e9c8dd149850da6e38253faceb5d763ace0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2842004bd43adbf1e1528d71d218625a5ced90")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantineInterfaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantineInterfaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
