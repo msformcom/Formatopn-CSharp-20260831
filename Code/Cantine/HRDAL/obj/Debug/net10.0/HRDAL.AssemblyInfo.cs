9add89a38a1522736d2d64a0b591c38d978703ce
chaminade <thibault.chaminade@aplim.fr>
2026-09-16 16:16:36 +0200

    config

--- Code/Cantine/HRDAL/obj/Debug/net10.0/HRDAL.AssemblyInfo.cs
+++ Code/Cantine/HRDAL/obj/Debug/net10.0/HRDAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRDAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ec3821d10ad8d3a8d5f5b6984ecb03db446d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891a0bc07aaa24e6978b4af95a63d4bfc1c37459")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRDAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRDAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
